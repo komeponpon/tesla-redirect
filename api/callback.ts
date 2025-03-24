bd5a7461dd7fa91e8dee38d8871bf25fe7ba8479
komeponpon <ricemountain1030@gmail.com>
2025-03-24 15:13:31 +0900

    initial commit

---
+++ api/callback.ts
@@ -0,0 +1,16 @@
+import type {VercelRequest, VercelResponse} from "@vercel/node"
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { code, state } = req.query;
+
+  if (!code || typeof code !== "string") {
+    return res.status(400).send("Missing or invalid code");
+    return;
+  }
+
+  const redirectUri = `myapp://auth?code=${encodeURIComponent(code)}${
+    state && typeof state === "string" ? `&state=${encodeURIComponent(state)}` : ""
+  }`;
+  res.writeHead(302, {Location: redirectUri});
+  res.end();
+}
